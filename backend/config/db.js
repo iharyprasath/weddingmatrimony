8a7e22b8370446bf3e0b87a060ee7566f2f66637
Hariprasath <harimass815@gmail.com>
2026-06-03 03:14:26 -0700

    ...

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "matrimony",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+  process.env.DB_NAME ,
+  process.env.DB_USER ,
+  process.env.DB_PASSWORD ,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST ,
     dialect: "mysql",
-    port: Number(process.env.DB_PORT || 3306),
+    port: Number(process.env.DB_PORT),
   },
 );
 
